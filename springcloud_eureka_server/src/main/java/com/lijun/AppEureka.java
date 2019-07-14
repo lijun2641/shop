cbdbcb982cad3a9b670d5cf3155494b8395ed9af
lijun2641 <389299811@qq.com>
2019-07-15 00:57:01 +0800

    lj

---
+++ springcloud_eureka_server/src/main/java/com/lijun/AppEureka.java
@@ -0,0 +1,15 @@
+package com.lijun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class AppEureka {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppEureka.class, args);
+	}
+
+}
